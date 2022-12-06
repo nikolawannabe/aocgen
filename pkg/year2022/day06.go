d78fa4f6c5e5a460680ad56d7a6aab9fd7d084c4
Case Schmidt <case.schmidt@draft2digital.com>
2022-12-05 21:33:14 -0800

    little refactor

--- pkg/year2022/day06.go
+++ pkg/year2022/day06.go
@@ -2,23 +2,27 @@ package year2022
 
 type Day06 struct{}
 
+func findMarker(runes []rune, markerLen int) int {
+	maybeSig := make([]rune, 0)
+	for n, char := range runes {
+		maybeSig = append(maybeSig, char)
+		setLen := len(set(maybeSig))
+		if setLen == markerLen {
+			return n + 1
+		}
+		if len(maybeSig) > markerLen-1 {
+			maybeSig = maybeSig[1:]
+		}
+	}
+	return 0
+}
+
 func (p Day06) PartA(lines []string) any {
 	sop := 4
 	input := lines[0]
 	runes := split(input)
 
-	maybeSig := make([]rune, 0)
-	for n, char := range runes {
-		maybeSig = append(maybeSig, char)
-		setLen := len(set(maybeSig))
-		if setLen == sop {
-			return n + 1
-		}
-		if len(maybeSig) > sop-1 {
-			maybeSig = maybeSig[1:]
-		}
-	}
-	return 0
+	return findMarker(runes, sop)
 }
 
 func (p Day06) PartB(lines []string) any {
@@ -26,16 +30,5 @@ func (p Day06) PartB(lines []string) any {
 	input := lines[0]
 	runes := split(input)
 
-	maybeSig := make([]rune, 0)
-	for n, char := range runes {
-		maybeSig = append(maybeSig, char)
-		setLen := len(set(maybeSig))
-		if setLen == som {
-			return n + 1
-		}
-		if len(maybeSig) > som-1 {
-			maybeSig = maybeSig[1:]
-		}
-	}
-	return 0
+	return findMarker(runes, som)
 }
